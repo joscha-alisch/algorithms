9a49679d9b267dc086314e1ca4bc8be2c19e09a7
Joscha Alisch <joscha.alisch@gmail.com>
2020-02-08 10:35:38 +0100

    Refactor swap

--- algorithms/sorting/selection_sort/solutions/kotlin/src/main/kotlin/me/alisch/algorithms/sorting/selectionsort/SelectionSort.kt
+++ algorithms/sorting/selection_sort/solutions/kotlin/src/main/kotlin/me/alisch/algorithms/sorting/selectionsort/SelectionSort.kt
@@ -1,19 +1,22 @@
 package me.alisch.algorithms.sorting.selectionsort
 
 fun <T: Comparable<T>> sort(array: Array<T>): Array<T> {
-
     (array.indices).forEach { index ->
-        var min = index
+        var minIndex = index
         (index until array.size).forEach { current ->
-            if(array[current] < array[min]) {
-                min = current
+            if(array[current] < array[minIndex]) {
+                minIndex = current
             }
         }
 
-        val temp = array[index]
-        array[index] = array[min]
-        array[min] = temp
+        array.swap(minIndex, index)
     }
 
     return array
+}
+
+private fun <T> Array<T>.swap(index1: Int, index2: Int) {
+    val temp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = temp
 }
