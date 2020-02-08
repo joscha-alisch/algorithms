be9772600dbbb0e720e775391e35cea971bdd21d
Joscha Alisch <joscha.alisch@gmail.com>
2020-02-08 11:49:33 +0100

    Fix typo

--- algorithms/sorting/insertion_sort/solutions/kotlin/attempt.sh
+++ algorithms/sorting/insertion_sort/solutions/kotlin/attempt.sh
@@ -6,4 +6,4 @@ IFS=$'\n\t'
 readonly BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$BASE_DIR"
 
-"$(git rev-parse --show-toplevel)"/helper/attempt.sh insertionsortt kotlin
+"$(git rev-parse --show-toplevel)"/helper/attempt.sh insertionsort kotlin
