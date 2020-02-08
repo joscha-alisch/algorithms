29bc7feb62722afaa4cce54c00e03166c8903a6f
Joscha Alisch <joscha.alisch@gmail.com>
2020-02-08 10:40:30 +0100

    Refactor finding the min

--- algorithms/sorting/selection_sort/solutions/kotlin/src/main/kotlin/me/alisch/algorithms/sorting/selectionsort/SelectionSort.kt
+++ algorithms/sorting/selection_sort/solutions/kotlin/src/main/kotlin/me/alisch/algorithms/sorting/selectionsort/SelectionSort.kt
@@ -2,17 +2,21 @@ package me.alisch.algorithms.sorting.selectionsort
 
 fun <T: Comparable<T>> sort(array: Array<T>): Array<T> {
     (array.indices).forEach { index ->
-        var minIndex = index
-        (index until array.size).forEach { current ->
-            if(array[current] < array[minIndex]) {
-                minIndex = current
-            }
-        }
-
+        val minIndex = array.indexOfMin(startingPoint = index)
         array.swap(minIndex, index)
     }
 
     return array
+}
+
+private fun <T: Comparable<T>> Array<T>.indexOfMin(startingPoint: Int = 0): Int {
+    var minIndex = startingPoint
+    (startingPoint until size).forEach { current ->
+        if(this[current] < this[minIndex]) {
+            minIndex = current
+        }
+    }
+    return minIndex
 }
 
 private fun <T> Array<T>.swap(index1: Int, index2: Int) {
